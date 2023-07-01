3d46137067c88767e1e71f7ec20a69e5cc5a7179
DenysFullstack <allcarinae@gmail.com>
2023-07-01 20:22:32 +0300

    update js

--- lesson-1/module-1.js
+++ lesson-1/module-1.js
@@ -33,3 +33,63 @@
 // for, while
 
 // switch
+
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+
+// const num = 6;
+// let result = "";
+
+// switch (num) {
+//   case 1:
+//     result = "зима";
+//     break;
+
+//   case 2:
+//     result = "весна";
+//     break;
+
+//   case 3:
+//     result = "літо";
+//     break;
+
+//   case 4:
+//     result = "осінь";
+//     break;
+
+//   default:
+//     result = "Такої пори року не існує";
+//     break;
+// }
+
+// console.log(result);
+
+// Напишіть if..else, що відповідає наступному switch:
+
+const browser = 'Opera';
+
+// switch (browser) {
+//   case 'Edge':
+//     alert("You've got the Edge!");
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert('Okay we support these browsers too');
+//     break;
+
+//   default:
+//     alert('We hope that this page looks ok!');
+// }
+
+if (browser==='Edge') {
+    alert("You've got the Edge!");
+} else if (browser==='Chrome'||browser==='Firefox'||browser==='Safari'||browser==='Opera') {
+    alert('Okay we support these browsers too');
+}
+else {
+    alert('We hope that this page looks ok!');
+}
