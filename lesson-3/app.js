0c11b4239fe762ff504ab28236b800ad71394a3f
Dmytro Sayko <saykodmytro@icloud.com>
2023-07-29 18:47:36 +0300

    D_S

--- lesson-3/app.js
+++ lesson-3/app.js
@@ -17,3 +17,25 @@
 //   console.log(`Привіт ${name}`);
 // }
 // letMeSeeYourName(greet);
+
+// TODO:============task-02=========================
+// Напишіть функцію, яка приймає два числа і колбек-функцію. Функція повинна помножити числа між собою та передати результат дії до колбек-функції. Колбек-функція повинна, у разі, якщо, це число є парним - помножити це число на себе, якщо ні - взяти квадратний корінь з цього числа, та вивести результат у консоль лог
+
+// function first(a, b, callback) {
+//   let c = a * b;
+//   return callback(c);
+// }
+
+// const first = (a, b, callback) => callback(a * b);
+
+// const callback = (num) => (num % 2 === 0 ? Math.pow(num, 2) : Math.sqrt(num));
+
+// // function callback(num) {
+// //     if (num % 2 === 0) {
+// //       return Math.pow(num, 2);
+// //     } else {
+// //       return Math.sqrt(num);
+// //     }
+// //   };
+
+// console.log(first(3, 3, callback));
