23a71d52a75cb0b35a1f25a77cdb7d081782a296
Dmytro Sayko <saykodmytro@icloud.com>
2023-08-12 19:02:03 +0300

    Task-1-3

--- lesson-4/module-6.js
+++ lesson-4/module-6.js
@@ -8,49 +8,108 @@
  * яка повертатиме масив елементів(масив з лішками).
  */
 
-const appendFruitList = ['apple🍎', 'banana🍌'];
-const prependFruitList = ['grapes🍇', 'orange🍊'];
-const beforeFruitList = ['strawberry🍓', 'cherry🍒'];
-const afterFruitList = ['peach🍑', 'kiwi🥝'];
+const appendFruitList = ["apple🍎", "banana🍌"];
+const prependFruitList = ["grapes🍇", "orange🍊"];
+const beforeFruitList = ["strawberry🍓", "cherry🍒"];
+const afterFruitList = ["peach🍑", "kiwi🥝"];
+
+const ulElement = document.createElement("ul");
+title.after(ulElement);
+
+function elementsOfIngridients(array) {
+  return array.map((el) => {
+    const liEl = document.createElement("li");
+    liEl.textContent = el;
+    return liEl;
+  });
+}
+
+ulElement.append(...elementsOfIngridients(appendFruitList));
+ulElement.prepend(...elementsOfIngridients(prependFruitList));
+ulElement.before(...elementsOfIngridients(beforeFruitList));
+// ulElement.after(...elementsOfIngridients(afterFruitList));
 
 //TODO:=========task-02=================
 // 3. Вставте даний текст в html документ, а потім виділіть усі слова, які мають більше 8 символів у тексті абзацу (наприклад, жовтим фоном).
 
 const text = `Об'єктна модель документа (Document Object Model) - незалежний від мови інтерфейс для роботи з HTML-документом. Містить набір властивостей і методів, що дозволяють шукати, створювати і видаляти елементи, реагувати на дії користувача і багато іншого. Тобто з'єднує сторінку з мовою програмування.`;
 
+const pElem = document.createElement("p");
+// pElem.textContent = text;
+ulElement.after(pElem);
+
+function textChek() {
+  const world = text.split(/\s+/);
+  world.forEach((el) => {
+    if (el.length > 8) {
+      const spanEl = document.createElement("span");
+      spanEl.style.backgroundColor = "yellow";
+      spanEl.textContent = el + " ";
+      pElem.appendChild(spanEl);
+    } else {
+      pElem.innerHTML += el + " ";
+    }
+  });
+}
+textChek();
+
 //TODO:=========task-03=================
 // Створіть контейнер div (з класом numberContainer )в HTML-документі та динамічно створіть 100 блоків (з класом number) наповнивши їх рандомними числами від 1 до 100 і додайте їх до контейнера div(numberContainer). Парні числа повинні мати зелений фон (додати клас even), Непарні числа - жовтий фон (додати клас odd).
 
+const divContainer = document.createElement("div");
+divContainer.classList.add("number-container");
 const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+let divArr = [];
+
+for (let index = 0; index < 100; index += 1) {
+  const divEl = document.createElement("div");
+  divEl.classList.add("number");
+  divEl.textContent = randomNumber();
+  divEl.textContent % 2 === 0
+    ? divEl.classList.add("even")
+    : divEl.classList.add("odd");
+
+  // if (divEl.textContent % 2 === 0) {
+  //   divEl.classList.add("even");
+  // } else {
+  //   divEl.classList.add("odd");
+  // }
+  // divContainer.appendChild(divEl);
+  divArr.push(divEl);
+}
+
+divContainer.append(...divArr);
+
+pElem.after(divContainer);
 
 // //TODO:=========task-04=================
 // Створи HTML список фільмів на основі масиву під назвою films. Користувач обожнює дивитися кіно, дивиться його часто, тому при вході на сторінку він хоче бачити, які фільми вже були переглянуті, а до яких він ще не добрався. Вперу чергу зарендери фільми на сторінку, використовуй допоміжну функцію createMarkup(), яка буде повертати розмітку. Додай розмітку на сторінку задопомогою insertAdjacentHTML() і тільки після цього зроби елементи фільмів (li), які вже були переглянуті напівпрозорими (opacity: 0.5). Для цього використовуй масив унікальних id фільмів, знайди на сторінці елементи у яких id дорівнює тому id фільма, який користувач вже перелянув аби саме їх зробити напівпрозорими.
 
 const films = [
   {
-    title: 'Tetris',
-    imgUrl: 'https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg',
-    id: 'film_1',
+    title: "Tetris",
+    imgUrl: "https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg",
+    id: "film_1",
   },
   {
-    title: 'Avatar: The Way of Water',
-    imgUrl: 'https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg',
-    id: 'film_2',
+    title: "Avatar: The Way of Water",
+    imgUrl: "https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg",
+    id: "film_2",
   },
   {
-    title: 'Operation Fortune: Ruse de guerre',
-    imgUrl: 'https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg',
-    id: 'film_3',
+    title: "Operation Fortune: Ruse de guerre",
+    imgUrl: "https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg",
+    id: "film_3",
   },
   {
-    title: 'Babylon',
-    imgUrl: 'https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg',
-    id: 'film_4',
+    title: "Babylon",
+    imgUrl: "https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg",
+    id: "film_4",
   },
   {
-    title: 'The Whale',
-    imgUrl: 'https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg',
-    id: 'film_5',
+    title: "The Whale",
+    imgUrl: "https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg",
+    id: "film_5",
   },
 ];
 
@@ -66,7 +125,7 @@ const films = [
 
 // function createMarkup() {}
 
-const watchedFilms = ['film_1', 'film_3', 'film_5'];
+const watchedFilms = ["film_1", "film_3", "film_5"];
 
 //TODO:=========task-05=================
 /**
@@ -92,7 +151,7 @@ const watchedFilms = ['film_1', 'film_3', 'film_5'];
 //TODO:===========3===============
 
 //TODO:===========4===============
-const spanEl = document.querySelector('.js-username-output');
+const spanEl = document.querySelector(".js-username-output");
 
 //TODO:=========task-06=================
 // Кнопка "Приховати" ховає текст і замінює назву кнопки на "Розкрити", у разі повторного натискання текст знову стає доступним і кнопка набуває початкового вигляду.
